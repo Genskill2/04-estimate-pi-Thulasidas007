5e5318bea783d81411e1a3d2fe083599ee61a4f2
Thulasidas007 <84675551+Thulasidas007@users.noreply.github.com>
2021-06-04 11:53:45 +0530

    Update monte_carlo.c

--- monte_carlo.c
+++ monte_carlo.c
@@ -38,16 +38,21 @@ int main(void) {
     }
   }
 }
-float mc_pi(int n) {
-float num=0.0,x,y;
-for(int i=1;i<=n;i++) {
+float mc_pi(int n)
+{	
+	int pc=0,ps=n;
+	
+	for(int i=0; i<n; i++){
+	float x,y,len;
 	x=frandom();
 	y=frandom();
-	if(sqrt((x*x)+(y*y))<=1) {
-	num++;
+	len =x*x+y*y;
+	if(len<=1){
+	pc++;
 	}
-}
-return 4.0*(num/(float)n);
+	}
+	float pi = 4.0*pc/ps;
+	return pi;
 }
 
 
